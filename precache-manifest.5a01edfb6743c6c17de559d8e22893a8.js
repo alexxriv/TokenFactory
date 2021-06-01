321bc22c374c3dfdda22292b9f0e8327172019c9
Kvothe <criptoeconomiaorg@protonmail.com>
2021-05-31 19:09:53 -0500

    Updates

---
+++ precache-manifest.5a01edfb6743c6c17de559d8e22893a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada319ba79ad98e987c1a2b88a5fed91",
+    "url": "/TokenFactory/index.html"
+  },
+  {
+    "revision": "f61a0bc5454d4102baaf",
+    "url": "/TokenFactory/static/css/main.7a6fe2f4.chunk.css"
+  },
+  {
+    "revision": "c73aae9352531f2ba7b9",
+    "url": "/TokenFactory/static/js/2.e88a8504.chunk.js"
+  },
+  {
+    "revision": "f61a0bc5454d4102baaf",
+    "url": "/TokenFactory/static/js/main.ac9b1545.chunk.js"
+  },
+  {
+    "revision": "290b9d1979e8e28d6567",
+    "url": "/TokenFactory/static/js/runtime-main.da9181bc.js"
+  }
+]);
